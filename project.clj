233a790fb16c41a2c17aad7d40cf758be3928d78
ingesolvoll <inge.solvoll@gmail.com>
2017-02-26 21:41:23 +0100

    Trying to get build working again

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.89" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.494" :scope "provided"]
                  [com.cognitect/transit-clj "0.8.285"]
                  [ring "1.4.0"]
                  [ring/ring-defaults "0.2.0"]
@@ -42,34 +42,35 @@
   :repl-options {:init-ns user}
 
   :cljsbuild {:builds
-              [{:id "app"
+              [{:id           "app"
                 :source-paths ["src/cljs" "src/cljc"]
 
-                :figwheel true
+                :figwheel     true
                 ;; Alternatively, you can configure a function to run every time figwheel reloads.
                 ;; :figwheel {:on-jsload "ls.core/on-figwheel-reload"}
 
-                :compiler {:main ls.core
-                           :asset-path "js/compiled/out"
-                           :output-to "resources/public/js/compiled/ls.js"
-                           :output-dir "resources/public/js/compiled/out"
-                           :source-map-timestamp true}}
+                :compiler     {:main                 ls.core
+                               :asset-path           "js/compiled/out"
+                               :output-to            "resources/public/js/compiled/ls.js"
+                               :output-dir           "resources/public/js/compiled/out"
+                               :source-map-timestamp true}}
 
-               {:id "test"
+               {:id           "test"
                 :source-paths ["src/cljs" "test/cljs" "src/cljc" "test/cljc"]
-                :compiler {:output-to "resources/public/js/compiled/testable.js"
-                           :main ls.test-runner
-                           :optimizations :none}}
+                :compiler     {:output-to     "resources/public/js/compiled/testable.js"
+                               :main          ls.test-runner
+                               :optimizations :none}}
 
-               {:id "min"
+               {:id           "min"
                 :source-paths ["src/cljs" "src/cljc"]
-                :jar true
-                :compiler {:main ls.core
-                           :output-to "resources/public/js/compiled/ls.js"
-                           :output-dir "target"
-                           :source-map-timestamp true
-                           :optimizations :advanced
-                           :pretty-print false}}]}
+                :jar          true
+                :compiler     {:main                 ls.core
+                               :output-to            "resources/public/js/compiled/ls.js"
+                               :output-dir           "target"
+                               :source-map-timestamp true
+                               :optimizations        :advanced
+                               :parallel-build       true
+                               :pretty-print         false}}]}
 
   ;; When running figwheel from nREPL, figwheel will read this configuration
   ;; stanza, but it will read it without passing through leiningen's profile
@@ -79,14 +80,14 @@
   :figwheel {;; :http-server-root "public"       ;; serve static assets from resources/public/
              ;; :server-port 3449                ;; default
              ;; :server-ip "127.0.0.1"           ;; default
-             :css-dirs ["resources/public/css"]  ;; watch and update CSS
+             :css-dirs       ["resources/public/css"]       ;; watch and update CSS
 
              ;; Instead of booting a separate server on its own port, we embed
              ;; the server ring handler inside figwheel's http-kit server, so
              ;; assets and API endpoints can all be accessed on the same host
              ;; and port. If you prefer a separate server process then take this
              ;; out and start the server with `lein run`.
-             :ring-handler user/http-handler
+             :ring-handler   user/http-handler
 
              ;; Start an nREPL server into the running figwheel process. We
              ;; don't do this, instead we do the opposite, running figwheel from
@@ -113,15 +114,15 @@
                              [com.cemerick/piggieback "0.2.1"]
                              [org.clojure/tools.nrepl "0.2.12"]]
 
-              :plugins [[lein-figwheel "0.5.8"]
-                        [lein-doo "0.1.6"]]
+              :plugins      [[lein-figwheel "0.5.8"]
+                             [lein-doo "0.1.6"]]
 
               :source-paths ["dev"]
               :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
 
              :uberjar
              {:source-paths ^:replace ["src/clj" "src/cljc"]
-              :prep-tasks ["compile" ["cljsbuild" "once" "min"]]
-              :hooks []
-              :omit-source true
-              :aot :all}})
+              :prep-tasks   ["compile" ["cljsbuild" "once" "min"]]
+              :hooks        []
+              :omit-source  true
+              :aot          :all}})
